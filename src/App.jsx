7b23d9e683e8f9bf5795d90e64137855c65839ec
Ankit Singh <ankit@bluparrot.in>
2026-01-07 17:16:00 +0530

    api intigration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [stockData, setStockData] = useState(null);
 
+  async function fetchData() {
+    try {
+      const response = await axios.get("http://localhost:5087/api/stocks");
+      setStockData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching stock data:", error);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Stock Data</h1>
+      
+      {stockData ? (
+        <div>
+          {stockData.map((stock) => (
+            <div key={stock.id} style={{ marginBottom: "20px" }}>
+              <p>
+                <strong>ID:</strong> {stock.id}
+              </p>
+              <p>
+                <strong>Name:</strong> {stock.companyName}
+              </p>
+              <p>
+                <strong>Price:</strong> ${stock.purchase}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Not found</p>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
